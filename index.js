5de2ffde2d7ac9dbd80215ea4354498cb3d92f3d
MD Pahlovi <mdpahlovi07@gmail.com>
2022-11-03 00:32:58 +0600

    Server  Connect Done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient } = require("mongodb");
+require("colors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,5 +13,15 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("E-Paq Server Running");
 });
+
+const url = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.ifnkrk6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(url);
+const fun = async () => {
+    await client.connect();
+    const products = client.db("e-paq").collection("products");
+    products.insertOne({ name: "Hanna" });
+};
+
+fun().catch((error) => console.log(`${error.name.bgRed.bold} ${error.message.red}`));
 
 app.listen(port);
