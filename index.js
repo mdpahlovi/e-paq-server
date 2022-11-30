f65e7c9906a47edeb0be18e4992fed85024a4701
MD Pahlovi <mdpahlovi07@gmail.com>
2022-11-30 22:05:01 +0600

    Get All Product & Pagination API added

--- index.js
+++ index.js
@@ -16,16 +16,34 @@ app.get("/", (req, res) => {
 
 const url = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.ifnkrk6.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(url);
-const fun = async () => {
-    await client.connect();
+
+const database = async () => {
     const productColection = client.db("e-paq").collection("products");
+
+    // Get All Product With Pagination
     app.get("/products", async (req, res) => {
+        const page = parseInt(req.query.page);
+        const size = parseInt(req.query.size);
         const curser = productColection.find({});
-        const products = await curser.toArray();
+        const products = await curser
+            .skip(page * size)
+            .limit(size)
+            .toArray();
+        const count = await productColection.estimatedDocumentCount();
+        res.send({ count, products });
+    });
+
+    // Products By Id
+    app.post("/productsByIds", async (req, res) => {
+        const ids = req.body;
+        const objectIds = ids.map((id) => ObjectId(id));
+        const query = { _id: { $in: objectIds } };
+        const cursor = productColection.find(query);
+        const products = await cursor.toArray();
         res.send(products);
     });
 };
 
-fun().catch((error) => console.log(`${error.name.bgRed.bold} ${error.message.red}`));
+database().catch(({ name, message }) => console.log(`${name.bgRed.bold} : ${message.red}`));
 
 app.listen(port);
